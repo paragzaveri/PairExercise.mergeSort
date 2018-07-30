a68dfb5a0d279089709a05dc8c93b021da90bd32
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-07-30 15:42:52 -0500

    Working Solution to Merge Sort

--- mergesort.js
+++ mergesort.js
@@ -1,89 +1,50 @@
-// function split(array) {
-
-//   if (array.length === 1) {
-//     return array;
-//   }
-
-//   let firstHalf = array.slice(0, Math.floor(array.length/2));
-//   let secondHalf = array.slice(Math.floor(array.length/2), array.length);
-
-//   // return [firstHalf, secondHalf];
-
-
-//   return [split(firstHalf), split(secondHalf)] ;
-
-// }
-
-// function merge(array1, array2){
-//   let mergedArray = [];
-//   for (let i = 0; i < array1.length; i++){
-//     mergedArray.push(array1[i]);
-//   }
-//   for (let i = 0; i < array2.length; i++){
-//     mergedArray.push(array2[i]);
-//   }
-
-//   return mergedArray;
-// }
-
 function mergeSort(array) {
+
+  if (array.length){
+    split(array);
+  }
+  return array;
+
+}
+
+function split(array){
   if (array.length === 1) {
     return;
   }
+  let leftArray = array.slice(0, Math.floor(array.length/2));
+  let rightArray = array.slice(Math.floor(array.length/2));
 
-  leftArray = array.slice(0, Math.floor(array.length/2));
-  rightArray = array.slice(Math.floor(array.length/2), array.length);
-
-  mergeSort(leftArray);
-  mergeSort(rightArray);
+  split(leftArray);
+  split(rightArray);
 
   merge(leftArray, rightArray, array);
 
   return array;
-
 }
 
-function merge(leftArray, rightArray) {
-  let array = [];
-  //merge(a1,a2);
-
-  // let a3 = [];
-
-  // if (a1 < a2) {
-  //   a3.push(a1[0]);
-  //   a3.push(a2[0]);
-  // }
-  // else {
-  //   a3.push(a2[0]);
-  //   a3.push(a1[0]);
-  // }
-  // return a3;
-
+function merge(leftArray, rightArray, array) {
   let index = 0;
-
 
   while(leftArray.length && rightArray.length){
     if (rightArray[0] < leftArray[0]) {
-      array[index++] = rightArray.shift();
+      array[index] = rightArray.shift();
     } else {
-      array[index++] = leftArray.shift();
+      array[index] = leftArray.shift();
     }
+    index++;
   }
 
   while (leftArray.length) {
-    array[index++] = leftArray.shift();
+    array[index] = leftArray.shift();
+    index++;
   }
 
   while (rightArray.length) {
-    array[index++] = rightArray.shift();
+    array[index] = rightArray.shift();
+    index++;
   }
 
   return array;
 
 }
 
-
-
-
-// split(arr.slice(0, Math.floor(arr.length/2)));
-// split(arr.slice(Math.floor(arr.length/2), arr.length));
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -17,6 +17,6 @@ describe('Split Array function', function() {
 
 describe('Merge function', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect( merge([3,8,6], [4,9,1]).toEqual([3,8,6,4,9,1]) );
+    expect( merge([3,8,6], [4,9,1], [3,8,6,4,9,1]).toEqual([1,3,4,6,8,9]) );
   });
 });
